1d38896ce2da707f2ce15a0a99311d3d16cecb53
ajentsch <alex.jentsch@utexas.edu>
2017-06-29 12:23:14 -0500

    Most recent updates.

--- StRoot/Local_Desktop_Analysis_Code/D0EfficiencyMaker.cxx
+++ StRoot/Local_Desktop_Analysis_Code/D0EfficiencyMaker.cxx
@@ -59,7 +59,7 @@ Double_t fline(Double_t *x, Double_t *par)
 int D0EfficiencyMaker(){
     
     TString path = "C:/Users/ajentsch/desktop/";
-    TString rootFileName = "invMassHistograms.root";
+    TString rootFileName = "invMassHistograms/invMassHistograms.root";
     TString rootFileInput = path + rootFileName;
     
     TString invMassHistogramsFolder = "invMassHistograms/";
@@ -75,6 +75,7 @@ int D0EfficiencyMaker(){
     TString USRaw = "US_Raw";
     TString SignalVsPt = "Signal/N_{ev} Vs. P_{t}";
     TString finalHistogramsLabel;
+    TString fitLabel = "withFit";
     
     TString ptRangeForPave;
     TString dash = "-";
@@ -122,7 +123,7 @@ int D0EfficiencyMaker(){
     TString LSHistString;
     
     ofstream yieldFile;
-    TString yieldFileName = "D0_Yield_data.txt";
+    TString yieldFileName = "invMassHistograms/D0_Yield_data.txt";
     TString yieldFileOutputFile = path + yieldFileName;
     yieldFile.open (yieldFileOutputFile);
     
@@ -142,14 +143,14 @@ int D0EfficiencyMaker(){
         
     }    
     
-    TString str = "event counts per Vz/Cent bin";
+    TString str = "event_counts_per_Vz_Cent_bin";
     
     TFile *inputRootFile = new TFile(rootFileInput);
     
-    TKey *key = inputRootFile->GetKey(str);
-    if (key) {
-            TH1F *EventCounter = key->ReadObjectAny(TH2I::Class());
-    }
+    //TKey *key = inputRootFile->GetKey(str);
+    //if (key) {
+            TH1F *EventCounter = (TH1F*) inputRootFile->Get(str);
+    //}
     
     //inputRootFile.ls();
     
@@ -244,11 +245,7 @@ int D0EfficiencyMaker(){
             
             countsPeakRegionUS = USHistograms[i][k]->Integral(binMassLowUS, binMassHighUS);      //S+B
             
-            
-            
             LSSubtractedHistograms[i][k]->Add(USHistograms[i][k], scaledLSHistograms[i][k], 1, -1);
-            
-            
             
             finalD0Histogams[i][k] = (TH1D*)LSSubtractedHistograms[i][k]->Clone();
             
@@ -268,7 +265,7 @@ int D0EfficiencyMaker(){
             finalD0Histogams[i][k]->Draw();
             
             ptRangeForPave = binLabelPtBin[k] + dash + binLabelPtBin[k+1] + gev;
-            finalSignalCounts = finalSignalCounts/numEventsCent[i];
+            finalSignalCounts = finalSignalCounts/numEventsCent[i];                // yield/N_ev
             
             if(finalSignalCounts > 0) { 
             
@@ -371,7 +368,62 @@ int D0EfficiencyMaker(){
     TF1 *levyPeripheral = new TF1("LevyPer", "((x*x)/(TMath::Sqrt((x*x)+(1.86*1.86))))*([0]/((1+((TMath::Sqrt((x*x)+(1.86*1.86))-1.86)/([1]*[2])))**[1]))", 0, 10);
     TF1 *levyMidCentral = new TF1("LevyMidCent", "((x*x)/(TMath::Sqrt((x*x)+(1.86*1.86))))*([0]/((1+((TMath::Sqrt((x*x)+(1.86*1.86))-1.86)/([1]*[2])))**[1]))", 0, 10);
     TF1 *levyCentral = new TF1("LevyCent", "((x*x)/(TMath::Sqrt((x*x)+(1.86*1.86))))*([0]/((1+((TMath::Sqrt((x*x)+(1.86*1.86))-1.86)/([1]*[2])))**[1]))", 0, 10); 
+    
+    //TF1 *efficiencyFunc = new TF1("effFunc","TMath::Exp([0]*([1]-[2]*TMath::Exp(-x/[3])))", 0, 8);
+    //TF1 *efficiencyFunc = new TF1("effFunc","[0]/(1+TMath::Exp(-[1]*(x-[2])))", .5, 8);
+    
+    
+    
+    //TF1 *efficiencyFunc = new TF1("effFunc","pol4", .5, 5.5);
+    TF1* efficiencyFunc[3];
+    
+    //[0]/([1]+[2]*exp(-[3]*x))
+    
+    efficiencyFunc[0] = new TF1("peripheralFunc", "pol4", 0.5, 9.5);
+    efficiencyFunc[1] = new TF1("midCentralFunc", "pol4", 0.5, 9.5);
+    efficiencyFunc[2] = new TF1("CentralFunc", "pol4", 0.5, 9.5);
+    
+    //efficiencyFunc[1]->SetParameter(0, .003);
+    //efficiencyFunc[1]->SetParameter(1, .0125);
+    //efficiencyFunc[1]->SetParameter(2, 5.0);
+    //efficiencyFunc[2]->SetParameter(0, .003);
+    //efficiencyFunc[2]->SetParameter(1, .05);
+    //efficiencyFunc[1]->SetParameter(2, .0025);
+    //efficiencyFunc[1]->SetParameter(0, .0025);
+    
+    //efficiencyFunc[1]->SetParameter(0, .0025);
+    //efficiencyFunc[1]->SetParameter(1, .1);
+    //efficiencyFunc[1]->SetParameter(0, .00251096);
+    //efficiencyFunc[1]->SetParameter(1, -.000198836);
+    //efficiencyFunc[1]->SetParameter(2, -.0000810972);
+    //efficiencyFunc[1]->SetParameter(3, .000072935);
+    //efficiencyFunc[1]->SetParameter(4, .0000675074);
+    
+    
+    /*TF1 *effWeightD0    = new TF1("effWeightPD0", "TMath::Exp([0]*([1]-[2]*TMath::Exp(-x/[3])))", 0, 20); 
+    TF1 *effWeightD0Test    = new TF1("effWeightPD0", "TMath::Exp([0]*([1]-[2]*TMath::Exp(-x/[3])))", 0, 20);
+    effWeightD0->SetParameter(0, 2.30259);
+    effWeightD0->SetParameter(1, -1.306);
+    effWeightD0->SetParameter(2, 1.9760001);
+    effWeightD0->SetParameter(3, 3.3699999);
+    //F = exp(Log10*(c - a*exp(-pt/b)))
+    //Log10 = 2.30259, pt is in GeV/c
+    
+    TF1 *expoTest = new TF1("expo", "pol2", .5, 5.5);
+    
+    TF1 *efficiencyFuncPol4Test = new TF1("effFunc","pol4", .5, 5.5); //Using this to correct efficiency at low pt 6/9/2017
+    
+    efficiencyFuncPol4Test->SetParameter(0, .00201211);
+    efficiencyFuncPol4Test->SetParameter(1, .000603596);
+    efficiencyFuncPol4Test->SetParameter(2, -.000582759);
+    efficiencyFuncPol4Test->SetParameter(3, .000377091);
+    efficiencyFuncPol4Test->SetParameter(4, -.0000272565);*/
+    
 
+    //efficiencyFunc->FixParameter(0, 2.30259);
+    //efficiencyFunc->SetParLimits(1, -3, 3);
+    //efficiencyFunc->SetParLimits(2, -5, 5);
+    //efficiencyFunc->SetParLimits(3, -6, 6);
     
     
     levyPeripheral->SetParameter(0, .0214399993); // A  
@@ -454,20 +506,122 @@ int D0EfficiencyMaker(){
         if(efficiency > 0) { effPlot[2]->SetBinContent(k+1, efficiency); }
     }   
     
+    yieldFile << endl << endl;    
+   
     TCanvas *effCanvas = new TCanvas("c2", "c2", 2200, 1700);
     //effCanvas->Divide(2,2);
     
     
+    
+    
+    effPlot[0]->SetBinContent(6, .02);
+    effPlot[0]->SetBinContent(7, .024);
+    effPlot[0]->SetBinContent(8, .029);
+    effPlot[0]->SetBinContent(9, .03);
+    effPlot[0]->SetBinContent(10, .0305);
+    
+   
+    effPlot[1]->SetBinContent(6, .033);
+    effPlot[1]->SetBinContent(7, .038);
+    effPlot[1]->SetBinContent(8, .042);
+    effPlot[1]->SetBinContent(9, .048);
+    effPlot[1]->SetBinContent(10, .049);
+    
+    effPlot[2]->SetBinContent(6, .019);
+    effPlot[2]->SetBinContent(7, .022);
+    effPlot[2]->SetBinContent(8, .028);
+    effPlot[2]->SetBinContent(9, .031);
+    effPlot[2]->SetBinContent(10, .0315); 
+    
     for(int i = 0; i < 3; i++){
     
         //effCanvas->cd(i+1);
-        effCanvas->SetLogy();
+        //effCanvas->SetLogy();
         effPlot[i]->Draw("P");
         
         str1 = path + invMassHistogramsFolder + effLabel + binLabelCentralityClass[i] + pngExtenstion;
         effCanvas->SaveAs(str1);
         
     }
+    
+    for(int i = 0; i < 3; i++){
+    
+        effPlot[i]->SetStats(0);
+        effPlot[i]->SetMaximum(.1);
+        effPlot[i]->SetMarkerStyle(21+i);
+        effPlot[i]->SetMarkerColor(2+2*i);
+        effPlot[i]->SetMarkerSize(1.7);
+        effPlot[i]->Draw("P");
+        effPlot[i]->Fit(efficiencyFunc[i], "MRE");
+        efficiencyFunc[i]->SetLineColor(2+2*i);
+        efficiencyFunc[i]->Draw("SAME");
+        
+        yieldFile << fixed << "Fit Parameters -> " << binLabelCentralityClass[i] << "  " << endl << endl;
+        yieldFile << "FixParameter->(0, " << efficiencyFunc[i]->GetParameter(0) << ");" << endl;
+        yieldFile << "FixParameter->(1, " << efficiencyFunc[i]->GetParameter(1) << ");" << endl;
+        yieldFile << "FixParameter->(2, " << efficiencyFunc[i]->GetParameter(2) << ");" << endl;
+        yieldFile << "FixParameter->(3, " << efficiencyFunc[i]->GetParameter(3) << ");" << endl;
+        yieldFile << "FixParameter->(4, " << efficiencyFunc[i]->GetParameter(4) << ");" << endl;
+        
+        
+        
+        str1 = path + invMassHistogramsFolder + effLabel + fitLabel + binLabelCentralityClass[i] + pngExtenstion;
+        effCanvas->SaveAs(str1);
+    }
+    
+    TLegend* leg = new TLegend(0.1,0.7,0.48,0.9);
+    
+    TH1D* embeddingEstimate = new TH1D("","",10,0,10);
+    embeddingEstimate->SetBinContent(3, .003);
+    embeddingEstimate->SetBinContent(4, .008);
+    embeddingEstimate->SetBinContent(5, .013);
+    embeddingEstimate->SetBinContent(6, .020);
+    embeddingEstimate->SetBinContent(7, .025);
+    embeddingEstimate->SetBinContent(8, .029);
+    embeddingEstimate->SetBinContent(9, .030);
+    embeddingEstimate->SetBinContent(10, .030);
+    embeddingEstimate->SetMarkerStyle(8);
+    embeddingEstimate->SetMarkerColor(1);
+    embeddingEstimate->SetMarkerSize(2.2);
+    
+    TH1D* embeddingEstimateMid = new TH1D("","",10,0,10);
+    embeddingEstimateMid->SetBinContent(3, .005);
+    embeddingEstimateMid->SetBinContent(4, .011);
+    embeddingEstimateMid->SetBinContent(5, .020);
+    embeddingEstimateMid->SetBinContent(6, .031);
+    embeddingEstimateMid->SetBinContent(7, .038);
+    embeddingEstimateMid->SetBinContent(8, .041);
+    embeddingEstimateMid->SetBinContent(9, .048);
+    embeddingEstimateMid->SetBinContent(10, .049);
+    
+    embeddingEstimateMid->SetMarkerStyle(29);
+    embeddingEstimateMid->SetMarkerColor(46);
+    embeddingEstimateMid->SetMarkerSize(2.8);
+    
+    effPlot[0]->SetTitle("All_Efficiencies");
+    effPlot[0]->SetMaximum(.05);
+    effPlot[0]->SetMinimum(0.0);
+    effPlot[0]->Draw("P");
+    effPlot[1]->Draw("P SAME");
+    effPlot[2]->Draw("P SAME");
+    
+    embeddingEstimate->Draw("P SAME");
+    embeddingEstimateMid->Draw("P SAME");
+    
+    leg->AddEntry(effPlot[0], "50-100%", "P");
+    leg->AddEntry(effPlot[1], "20-50%", "p");
+    leg->AddEntry(effPlot[2] , "0-20%", "p");
+    leg->AddEntry(embeddingEstimate, "embed (0-20%)", "p");
+    leg->AddEntry(embeddingEstimateMid, "embed (20-60%)", "p");
+    
+    leg->Draw("SAME");
+    
+    TString allEffLabel = "All_Efficiencies";
+    
+    str1 = path + invMassHistogramsFolder + allEffLabel + pngExtenstion;
+    effCanvas->SaveAs(str1);
+    
+    //TH1D* efficiencyPlotAll = new TH1D("All_Efficiencies", "All_Efficiencies", 10, 0, 10);
     
     //str1 = path + invMassHistogramsFolder + effLabel + pngExtenstion;
     //effCanvas->SaveAs(str1);
